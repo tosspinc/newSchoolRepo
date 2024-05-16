1a87404cb0b3ba8ef7ed2190f775377c75f23e78
arnold jones <tosspinc@gmail.com>
2024-05-16 01:41:36 -0600

    completed coding for basic express server. completed 05.16.2024 at 1308hours.

--- Level5/Lessons/1st-xpress-server/server.js
+++ Level5/Lessons/1st-xpress-server/server.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const app = express()   //app now has everything to become a server from express.
 
+//data to be used for now.
+const users = [
+    { name: "Joe", age: 30 },
+    { name: "Sam", age: 29 },
+    { name: "Barbara", age: 48 },
+    { name: "Jackie", age: 32 },
+    { name: "Bill", age: 60 },
+]
+
 //first argument is the endpoint (mount path)
 //second argument is the callback function
-app.get("/", (req, res) => {
-    res.send({name: "Joe", age: 30})
+app.get("/users", (req, res) => {
+    res.send(users)
 }) 
 
 
 app.listen(9000, () => {
     console.log("Server is running on port: 9000")
-})
+})  
