1b9b954e4a5385e345d3bff64f89163e86b7c210
arnold jones <tosspinc@gmail.com>
2024-05-21 14:29:56 -0600

    completed coding for adding new bounty, updating a bounty and deleting a bounty for level assignmentr Bounty Hunter 2. Completed on 05.21.2024 at 1430hours.

--- Level5/Practice/BountyHunter/routes/bountyHunterRouter.js
+++ Level5/Practice/BountyHunter/routes/bountyHunterRouter.js
@@ -1,54 +1,76 @@
 const express = require('express');
-const {v4 : uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const bountyHunterRouter = express.Router();
 
-
 const bountyHunter = [
-    { 
-        fName: "John", 
-        lName: "Smith", 
-        living: "true",
+    {
+        fName: "John",
+        lName: "Smith",
+        living: true,
         bountyAmt: 10000,
         type: "sith",
         _id: uuidv4()
     },
-    { 
-        fName: "Jane", 
-        lName: "Doe", 
-        living: "true",
+    {
+        fName: "Jane",
+        lName: "Doe",
+        living: true,
         bountyAmt: 1000,
         type: "jedi",
         _id: uuidv4()
     },
-    { 
-        fName: "Luke", 
-        lName: "Skywalker", 
-        living: "true",
+    {
+        fName: "Luke",
+        lName: "Skywalker",
+        living: true,
         bountyAmt: 100000,
         type: "jedi",
         _id: uuidv4()
     },
-    { 
-        fName: "Sandra", 
-        lName: "Bullock", 
-        living: "true",
+    {
+        fName: "Sandra",
+        lName: "Bullock",
+        living: true,
         bountyAmt: 250000,
         type: "sith",
         _id: uuidv4()
     }
-]
+];
 
-//routes
+// Update one
+bountyHunterRouter.put("/:bountyHunterId", (req, res) => {
+    const bountyHunterId = req.params.bountyHunterId;
+    const updateBounty = req.body;
+    const bountyIndex = bountyHunter.findIndex(bounty => bounty._id === bountyHunterId);
+    
+    if (bountyIndex === -1) {
+        return res.status(404).send("Bounty Hunter not found");
+    }
+    
+    const updatedBountyHunter = { ...bountyHunter[bountyIndex], ...updateBounty };
+    bountyHunter[bountyIndex] = updatedBountyHunter;
+
+    res.send(updatedBountyHunter);
+});
+
+//delete one
+bountyHunterRouter.delete("/:bountyHunterId", (req, res) => {
+    const bountyHunterId = req.params.bountyHunterId;
+    const bountyIndex = bountyHunter.findIndex(bounty => bounty._id === bountyHunterId)
+    bountyHunter.splice(bountyIndex, 1)
+    res.send("successfully deleted bounty!")
+})
+
+// Get all and create new
 bountyHunterRouter.route("/")
     .get((req, res) => {
-        res.send(bountyHunter)
+        res.send(bountyHunter);
     })
     .post((req, res) => {
-        const newBountyHunter = req.body
-        newBountyHunter._id = uuidv4()
-        bountyHunter.push(newBountyHunter)
-        res.send(`Successfully added ${newBountyHunter} to the database.`)
-    })
+        const newBountyHunter = req.body;
+        newBountyHunter._id = uuidv4();
+        bountyHunter.push(newBountyHunter);
+        res.send(`Successfully added ${newBountyHunter.fName} ${newBountyHunter.lName} to the database.`);
+    });
 
-module.exports = bountyHunterRouter
-    
+module.exports = bountyHunterRouter;
