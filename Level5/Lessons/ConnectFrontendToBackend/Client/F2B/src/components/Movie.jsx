bde9de059613f6ff868670ba98cd23267b94634d
arnold jones <tosspinc@gmail.com>
2024-05-22 15:27:53 -0600

    completed level 5 sp 355.25, get request to server. completed coding on 05.22.2024 at 1528hours.

--- Level5/Lessons/ConnectFrontendToBackend/Client/F2B/src/components/Movie.jsx
+++ Level5/Lessons/ConnectFrontendToBackend/Client/F2B/src/components/Movie.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function Movie(props) {
-    console.log(props)
+    const {title, genre, _id} = props
     return (
         <div>
             {/* <p>Hello World</p> */}
-            <h1>Title: {props.title}</h1>
-            <p>Genre: {props.genre}</p>
+            <h1>Title: {title}</h1>
+            <p>Genre: {genre}</p>
+            <p>ID: {_id}</p>
         </div>
     )
 }
