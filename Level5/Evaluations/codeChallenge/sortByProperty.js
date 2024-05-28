8decda30ffe39bd54ab2e50ad2d4e941ffaa33fa
arnold jones <tosspinc@gmail.com>
2024-05-28 13:10:41 -0600

    updated code to make simpler and more readable. completed 05.28.2024 at 1311hours.

--- Level5/Evaluations/codeChallenge/sortByProperty.js
+++ Level5/Evaluations/codeChallenge/sortByProperty.js
@@ -6,30 +6,10 @@ const people = [
     { name: "David", age: 28 }
 ]
 
-//create constant variable called sortedByAge and setting sortByProperty which passes two components to it.
-const sortedByAge = sortByProperty(people, 'age')
-
-//creates a function called sortByProperty and passes in two components.
-function sortByProperty(objects, propertyName) {
-    //creating a copy of the array so I do not mutate original. This slices apart the sortedArray data.
-    const sortedArray = objects.slice()
-
-    //sorting the array and it has two components.
-    sortedArray.sort((a, b) => {
-        //if propertyName a is less than propertyName b
-        if (a[propertyName] < b[propertyName]) {
-        //this puts object a object in front of b object data if a is smaller than b.
-        return -1
-        // this says if a object is greater than b object then put a object after b object.
-        } else if (a[propertyName] > b[propertyName]) {
-            //if a object and b object are equal or same then sort.
-            return 1
-        } else {
-            return 0
-        }
-    })
-    //sends sortedArray object data to console.
-    return sortedArray
+function sortByProperty(objects, PropertyName) {
+    return objects.slice().sort((a, b) => a[PropertyName] - b[PropertyName])
 }
 
-console.log(sortedByAge)
+const sortByAge = sortByProperty(people, "age")
+
+console.log(sortByAge)
