90fd6e0dd0f7d357fc7b5db0f8e8075c363b8335
arnold jones <tosspinc@gmail.com>
2024-05-28 12:43:28 -0600

    made code and notation easier to read.

--- Level5/Evaluations/codeChallenge/extractUniqueCharacters.js
+++ Level5/Evaluations/codeChallenge/extractUniqueCharacters.js
@@ -6,24 +6,17 @@ function extractUniqueCharacters(strings) {
     //Initialize variable uniqueCharsSet and setting to a class called Set().
     const uniqueCharsSet = new Set()
 
-    //Iterate over each string in the input array.
-    for (const str of strings) {
-        //iterate over each character in current string.
-        for (const char of str) {
-            //add character to Set
-            uniqueCharsSet.add(char)
+    for (const str of strings) {        //Iterate over each string in the input array.
+        for (const char of str) {       //iterate over each character in current string.
+            uniqueCharsSet.add(char)    //add character to Set
         }
     }
-    //converts uniqueCharsSet to an array and returns it.
-    return Array.from(uniqueCharsSet)
+    return Array.from(uniqueCharsSet)   //converts uniqueCharsSet to an array and returns it.
 }
+console.log(uniqueChars)//output should be: ['a', 'p', 'l', 'e', 'b', 'n', 'c', 'h', 'r', 'y']
 
-console.log(uniqueChars)
-
-//output should be: ['a', 'p', 'l', 'e', 'b', 'n', 'c', 'h', 'r', 'y']
 
 //another way is as follows and puts letter in alphabetical order:
-
 const fruits = ["apple", "banana", "cherry"]        //declares fruits as an array.
 let combineWords = fruits.join("")                  //joins all three fruit names into one string.
 let fruitsArray = combineWords.split("")            //splits apart the combineWords string
